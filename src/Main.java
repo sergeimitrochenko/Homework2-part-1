3bd9db08b23ac3e6b2f04643f88de57915686b40
sergeimitrochenko <aron.armenyan@yandex.ru>
2024-07-16 23:31:37 +0300

    "Домашняя работа 2 часть 1:задача 1 и 2"

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,33 @@ public class Main {
 
         System.out.println("Задача 1");
         var dog = 8;
-        var cat=3.6;
-        var paper =763789;
+        var cat = 3.6;
+        var paper = 763789;
         System.out.println("Значение переменной dog с типом var равно " + dog);
         System.out.println("Значение переменной cat с типом var равно " + cat);
         System.out.println("Значение переменной paper с типом var равно " + paper);
+
+
+        System.out.println("Задача 2");
+        var dog1 = 8;
+        var cat2 = 3.6;
+        var paper3 = 763789;
+        var box1=2;
+        var box2=2;
+        var box3=2;
+        System.out.println(" dog1=  " + dog1);
+        System.out.println(" cat2= " + cat2);
+        System.out.println(" paper3= " + paper3);
+        System.out.println(" box1= "+box1);
+        System.out.println(" box2= " +box2);
+        System.out.println(" box3=" + box3);
+        var result1 =dog1+box1+2;
+        var result2=cat2+box2+2;
+        var result3=paper3+box3+2;
+        System.out.println("dog1+box1+2="+result1);
+        System.out.println("cat2+box2+2=" +result2);
+        System.out.println("paper3+box3+2=" +result3);
+
+
     }
 }
