3d7f3f1ebaeff8925f3cb96724720657bb5e869c
sergeimitrochenko <aron.armenyan@yandex.ru>
2024-07-16 21:08:40 +0300

    "Домашняя работа 2 часть 1:задача 1"

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello people,check are out");
-        }
 
 
-
+        System.out.println("Задача 1");
+        var dog = 8;
+        var cat=3.6;
+        var paper =763789;
+        System.out.println("Значение переменной dog с типом var равно " + dog);
+        System.out.println("Значение переменной cat с типом var равно " + cat);
+        System.out.println("Значение переменной paper с типом var равно " + paper);
     }
+}
